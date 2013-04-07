67d3d32fc04c8449e9e916087273205d0fd244fe
Arthur Neuman <arthur.neuman@gmail.com>
2013-04-07 13:05:36 -0400

    Javascript doesn't allow ints as keys for associative arrays and thus when the data was dumped to JSON, ints became strings.  Rather than write crazy to deal with this, use YAML instead.

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ import sys
 
 import urllib.request
 
+import yaml
+
 def get_response(req_url, json_resp=True):
     """Get response from a sepecified URL; process JSON if response
     is JSON, else return data from URL request.
@@ -27,9 +29,11 @@ def get_response(req_url, json_resp=True):
 
 
 def load_file(path, default):
+    """Load JSON data from a file.  If the file does not exist, return
+    specified default value."""
     try:
         with open(path) as infile:
-            data = json.load(infile)
+            data = yaml.load(infile)
     except IOError:
         data = default
 
@@ -37,10 +41,16 @@ def load_file(path, default):
 
 
 def save_file(path, content):
+    """Save data to file using JSON"""
     with open(path, 'w') as outfile:
-        json.dump(content, outfile)
+        yaml.dump(content, stream=outfile)
 
 def time_compare(result):
+    """Compare the time, in UTC, of a result with the time one hour ago,
+    in UTC.  If the year, month, day, and hour are the same, return True
+    and the result time.
+    """
+
     compare = datetime.datetime.now(datetime.timezone.utc) -\
               datetime.timedelta(hours=1)
     compare_time = (compare.year, compare.month, compare.day, compare.hour)
@@ -53,7 +63,7 @@ def time_compare(result):
     return compare_time == result_compare, result_time
 
 class PolarStats():
-    """Container for data and associated methods."""
+    """Provides methods for loading, saving, updating, and plotting data."""
 
     def __init__(self, data_path=None, url=None):
         """Set path and source url, initialize data container."""
@@ -62,7 +72,7 @@ class PolarStats():
                           os.path.join(home, '.polar-stats')
         self._data_file_path = os.path.join(self._data_path, 'data')
         if not os.path.exists(self._data_path):
-            os.makedirs(self.data_path)
+            os.makedirs(self._data_path)
 
         self._url = url if url else "http://search.twitter.com/search.json"\
                     "?q=%22polar%20bear%22&result_type=mixed&rpp=100"\
@@ -84,15 +94,15 @@ class PolarStats():
                 result_has_date, result_time = time_compare(result)
                 if result_has_date:
                     has_date = True
-                    self._data['monthly'][str(result_time.month)] += 1
-                    self._data['hourly'][str(result_time.hour)] += 1
+                    self._data['monthly'][result_time.month] += 1
+                    self._data['hourly'][result_time.hour] += 1
 
             page += 1
 
     def load_data(self):
         """Load saved data from file."""
-        data_default = {'hourly': {str(x): 0 for x in range(24)},
-                        'monthly': {str(x): 0 for x in range(1, 13)}}
+        data_default = {'hourly': {x: 0 for x in range(24)},
+                        'monthly': {x: 0 for x in range(1, 13)}}
         self._data = load_file(self._data_file_path, data_default)
 
     def save_data(self):
@@ -100,35 +110,44 @@ class PolarStats():
         save_file(self._data_file_path, self._data)
 
     def update(self):
+        """Update data.  This will load the data file, download and process
+        results, and save the data file.
+        """
         if not self._data:
             self.load_data()
         self.get_polar_data()
         self.save_data()
 
     def generate_graphs(self):
-        """Generate and save plots from data."""
+        """Generate and save plots from data.  
+        See http://matplotlib.org/examples/pylab_examples/polar_bar.html 
+        for more details.
+        """
         Ns = {'hourly': 24, 'monthly': 12}
         for key in self._data.keys():
-            hour, values = zip(*data[key].items())
+            time, values = zip(*self._data[key].items())
             radius = max(values)
             values = [x / radius for x in values]           
             fig = pylab.figure()
-            ax = fig.add_axes(polar=True)
+            ax = fig.add_axes([1, 1, 1, 1], polar=True)
             N = Ns[key]
-            theta = numpy.arange(0.0, 2*numpy.pi, 2*numpy.pi / N)
-            width = np.pi / (2 * N)
-            bars = ax.bar(theta, radius, width=width, bottom=0.0)
-            fig.savefig(''.join([self._data_path, key, '.jpg']))
+            theta = pylab.arange(0.0, 2*pylab.pi, 2*pylab.pi / N)
+            width = pylab.pi / (2 * N)
+            bars = ax.bar(theta, values, width=width, bottom=0.0)
+            filename = "{key}.png".format(key=key)
+            filename = os.path.join(self._data_path, filename)
+            fig.savefig(filename)
 
 
 def main():
+    """Update data and generate new plots."""
     polar = PolarStats()
     try:
         polar.update()
     except urllib.error.HTTPError:
         print('unable to update', file=sys.stderr)
 
-    # polar.generate_graphs(DATA_PATH)
+    # polar.generate_graphs()
 
 if __name__ == "__main__":
     main()

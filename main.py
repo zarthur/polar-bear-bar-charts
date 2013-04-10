f90569bd6fc2c1d4a46da78f655e2359841ef427
Arthur Neuman <arthur.neuman@gmail.com>
2013-04-10 08:51:24 -0400

    removing polar class

--- main.py
+++ main.py
@@ -30,145 +30,134 @@ def get_response(req_url, json_resp=True):
     req_data = urllib.request.urlopen(request)
     data = json.loads(req_data.read().decode()) \
             if json_resp else req_data
+    
     return data
 
 
-def load_file(path, default):
-    """Load JSON data from a file.  If the file does not exist, return
-    specified default value."""
-    try:
-        with open(path) as infile:
-            data = yaml.load(infile)
-    except IOError:
-        data = default
-
-    return data
-
-
-def save_file(path, content):
-    """Save data to file using JSON"""
-    with open(path, 'w') as outfile:
-        yaml.dump(content, stream=outfile)
-
-
-def time_compare(result):
+def time_compare(result, current_time):
     """Compare the time, in UTC, of a result with the time one hour ago,
     in UTC.  If the year, month, day, and hour are the same, return True
     and the result time.
     """
 
-    compare = datetime.datetime.now(datetime.timezone.utc) -\
-              datetime.timedelta(hours=1)
-    compare_time = (compare.year, compare.month, compare.day, compare.hour)
-
+    compare_time = (current_time.year, current_time.month, current_time.day, 
+                    current_time.hour)
     result_time =  datetime.datetime.strptime(result['created_at'],
                                               '%a, %d %b %Y %H:%M:%S %z')
     result_compare = (result_time.year, result_time.month, result_time.day,
                       result_time.hour)
+    
     return compare_time == result_compare
 
 
-class PolarStats():
-    """Provides methods for loading, saving, updating, and plotting data."""
+def get_polar_data(data, url_template):
+    """Get reddit JSON data."""
+    # reset month total at the beginning of the month
+    current_time = datetime.datetime.now(datetime.timezone.utc) -\
+                   datetime.timedelta(hours=1)
+    if current_time.day == 1 and current_time.hour == 0:
+        data['monthly'][current_time.month] = 0
 
-    def __init__(self, data_path=None, url=None):
-        """Set path and source url, initialize data container."""
-        file_path = os.path.dirname(os.path.realpath(__file__))
-        self._data_path = data_path if data_path else file_path
-        self._data_file_path = os.path.join(self._data_path, 'data')
-
-        self._url = url if url else "http://search.twitter.com/search.json"\
-                    "?q=%22polar%20bear%22&result_type=mixed&rpp=100"\
-                    "&page={page}"
-
-        self._data = None
-
-    def get_polar_data(self):
-        """Get reddit JSON data."""
-        # reset month total at the beginning of the month
-        current_time = datetime.datetime.now(datetime.timezone.utc) -\
-                       datetime.timedelta(hours=1)
-        if current_time.day == 1 and current_time.hour == 0:
-            self._data['monthly'][current_time.month] = 0
-
-        page = 1
-        count = 0
-        while True:
-            try:
-                url = self._url.format(page=str(page))
-                response = get_response(url)['results']
-            except:
-                break
-                
-            count += len([result for result in response 
-                          if time_compare(result)])
-            page += 1
-        
-        self._data['monthly'][current_time.month] += count
-        self._data['hourly'][current_time.hour] = count
-
-    def load_data(self):
-        """Load saved data from file."""
-        data_default = {'hourly': {x: 0 for x in range(24)},
-                        'monthly': {x: 0 for x in range(1, 13)}}
-        self._data = load_file(self._data_file_path, data_default)
-
-    def save_data(self):
-        """Save data to file."""
-        save_file(self._data_file_path, self._data)
-
-    def update(self):
-        """Update data.  This will load the data file, download and process
-        results, and save the data file.
-        """
-        if not self._data:
-            self.load_data()
-        self.get_polar_data()
-        self.save_data()
-
-    def generate_graphs(self, path=None):
-        """Generate and save plots from data.  
-        See http://matplotlib.org/examples/pylab_examples/polar_bar.html 
-        for more details.
-        """
-        path = path if path else os.path.join(self._data_path, 'web')
-
-        titles = {'hourly': 'Hourly (UTC, Last 24 hours)',
-                  'monthly': 'Monthly (Last 12 months)'}
-
-        for key in self._data.keys():
-            time, values = zip(*self._data[key].items())
+    page = 1
+    count = 0
+    while True:
+        try:
+            url = url_template.format(page=str(page))
+            response = get_response(url)['results']
+        except:
+            break
             
-            fig = pylab.figure()
-            ax = fig.add_axes([0.1, 0.1, 0.8, 0.8], polar=True)
-            
-            N = len(values)
-            theta = pylab.arange(0.0, 2*pylab.pi, 2*pylab.pi / N)
-            width = pylab.pi / (N / 2) 
-            bars = ax.bar(theta - width /2 , values, width=width, bottom=0.0)
-            
-            for val, bar in zip(values, bars):
-                color = [random.random() for x in range(3)]
-                bar.set_facecolor(color)
+        count += len([result for result in response 
+                      if time_compare(result, current_time)])
+        page += 1
+    
+    data['monthly'][current_time.month] += count
+    data['hourly'][current_time.hour] = count
+    data['current_time'] = current_time
 
-            ax.set_title(titles[key])
-            ax.xaxis.set_major_locator(pylab.FixedLocator(theta))
-            ax.set_xticklabels(time)
-
-            filename = "{key}.png".format(key=key)
-            filename = os.path.join(path, filename)
-            fig.savefig(filename)
+    return data
 
 
-def main():
-    """Update data and generate new plots."""
-    polar = PolarStats()
+def load_data(data_path):
+    """Load saved data from file."""
+    data_default = {'hourly': {x: 0 for x in range(24)},
+                    'monthly': {x: 0 for x in range(1, 13)}}
+
     try:
-        polar.update()
+        with open(data_path) as infile:
+            data = yaml.load(infile)
+    except IOError:
+        data = data_default
+
+    return data
+
+
+def save_data(data, path):
+    """Save data to file."""
+    with open(path, 'w') as outfile:
+        yaml.dump(data, stream=outfile)
+
+
+def update(path, url):
+    """Update data.  This will load the data file, download and process
+    results, and save the data file.
+    """
+    data = load_data(path)
+    data = get_polar_data(data, url)
+    save_data(data, path)
+
+    return data
+
+
+def generate_graphs(data, path=None):
+    """Generate and save plots from data.  
+    See http://matplotlib.org/examples/pylab_examples/polar_bar.html 
+    for more details.
+    """
+    titles = {'hourly': 'Hourly (UTC, Last 24 hours)',
+              'monthly': 'Monthly (Last 12 months)'}
+    current_time = data.pop('current_time')
+    
+    for key in data.keys():
+        time, values = zip(*data[key].items())
+        
+        fig = pylab.figure()
+        ax = fig.add_axes([0.1, 0.1, 0.8, 0.8], polar=True)
+        
+        N = len(values)
+        theta = pylab.arange(0.0, 2*pylab.pi, 2*pylab.pi / N)
+        width = pylab.pi / (N / 2) 
+        bars = ax.bar(theta - width /2 , values, width=width, bottom=0.0)
+        
+        for val, bar in zip(values, bars):
+            color = [random.random() for x in range(3)]
+            bar.set_facecolor(color)
+
+        ax.set_title(titles[key])
+        ax.xaxis.set_major_locator(pylab.FixedLocator(theta))
+        ax.set_xticklabels(time)
+
+        filename = "{key}.png".format(key=key)
+        filename = os.path.join(path, filename)
+        fig.savefig(filename)
+
+
+def main(path=None, url=None):
+    path = path if path else os.path.dirname(os.path.realpath(__file__))
+    data_file_path = os.path.join(path, 'data')
+    web_path = os.path.join(path, 'web')
+
+    url = url if url else "http://search.twitter.com/search.json"\
+                "?q=%22polar%20bear%22&result_type=mixed&rpp=100"\
+                "&page={page}"
+
+    try:
+        data = update(data_file_path, url)
     except urllib.error.HTTPError:
         print('unable to update', file=sys.stderr)
+        sys.exit(1)
 
-    polar.generate_graphs()
+    generate_graphs(data, web_path)
 
 
 if __name__ == "__main__":
